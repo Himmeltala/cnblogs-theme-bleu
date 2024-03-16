Himmelbleu/cnblogs-theme-ecy

首页封面已完成

src/views/Index.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import { useBaseAuthorData } from "@/store";
+import { getHomeWritingList } from "@/apis/remote-api";
 
 EcyUtils.startLoading();
 
-const store = useBaseAuthorData();
-const authorData = shallowRef();
-const masterData = shallowRef();
+const setting = EcyUtils.getSetting();
+const listing = shallowRef(await getHomeWritingList(1));
 
-store.$onAction(({ args }) => {
-  authorData.value = args[0].author;
-  masterData.value = args[0].master;
-});
+async function fetchData(e: any) {
+  EcyUtils.startLoading();
+  listing.value = await getHomeWritingList(e.currentIndex);
+  EcyUtils.endLoading();
+}
 
-const router = useRouter();
+function moveToStartNail() {
+  document.querySelector("#l-start-nail").scrollIntoView();
+}
 
 onMounted(() => {
   EcyUtils.endLoading();
@@ -20,198 +22,112 @@ onMounted(() => {
 </script>
 
 <template>
-  <Card class="l-index min-height l-fiv-size">
-    <div class="l-index__content">
-      <div class="mb-6 f-s-c">
-        <div class="w-40%">
-          <div class="mb-6">
-            <div class="font-bold mb-2 title">{{ EcyConfig.blogApp }} 的博客</div>
-            <div :class="{ 'f-c-s': EcyConfig.pcDevice }">
-              <img
-                :class="{ 'mr-10': EcyConfig.pcDevice, 'mb-4': !EcyConfig.pcDevice }"
-                class="w-25 h-25 rd-50"
-                :src="EcyConfig.__ECY_CONFIG__.cabinet.avatar" />
-              <div>
-                <template v-if="authorData">
-                  <div class="mb-1 hover">圆龄：{{ authorData[1]?.text || "" }}</div>
-                  <div class="mb-1 hover">粉丝：{{ authorData[2]?.text || "" }}</div>
-                  <div class="mb-1 hover">关注：{{ authorData[3]?.text || "" }}</div>
-                </template>
-                <template v-if="masterData">
-                  <div class="mb-1 hover">随笔：{{ masterData[0]?.digg || "" }}</div>
-                  <div class="mb-1 hover">阅读：{{ masterData[3]?.digg || "" }}</div>
-                </template>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="mb-2 font-bold title">个人签名</div>
-            <div class="hover" v-html="EcyConfig.__ECY_CONFIG__.cabinet.signature"></div>
-          </div>
-        </div>
-        <div class="w-60% mb-6">
-          <div class="mb-2 font-bold title">个人简介</div>
-          <div v-html="EcyConfig.__ECY_CONFIG__.nameplate.intro"></div>
-        </div>
-      </div>
-      <div class="mb-6">
-        <div class="mb-4 font-bold title">个人标签</div>
-        <div class="hobbies f-c-s flex-wrap">
-          <LTag class="mr-2 mb-4" hover round v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.tags">{{ item }}</LTag>
-        </div>
-      </div>
-      <div v-if="EcyConfig.pcDevice" class="f-s-b mb-6">
-        <div class="w-50%">
-          <div class="mb-6">
-            <div class="mb-2 font-bold title">联系方式</div>
-            <div class="f-s-s">
-              <div class="mr-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.connection">
-                <el-tooltip :content="item.name + ': ' + item.text">
-                  <svg v-if="item.svg" class="w-6 h-6 hover" viewBox="0 0 1024 1024" v-html="item.svg"></svg>
-                  <img v-else class="rd-50 w-6 h-6 hover" alt="FAILED" :src="item.img" />
-                </el-tooltip>
-              </div>
-            </div>
-          </div>
-          <div class="mb-6">
-            <div class="mb-2 font-bold title">闲言碎语</div>
-            <div v-html="EcyConfig.__ECY_CONFIG__.nameplate.gossip"></div>
-          </div>
-          <div class="mb-6" v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-            <div class="mb-4 font-bold title">精选图片</div>
-            <div class="f-c-b flex-wrap">
-              <div v-for="(item, i) in EcyConfig.__ECY_CONFIG__.nameplate.photo.src" :class="{ ' h-28': i < 3 }" style="width: 32%">
-                <el-image
-                  v-if="i < 3"
-                  class="w-100% h-100%"
-                  :initial-index="i"
-                  :src="item"
-                  :preview-src-list="EcyConfig.__ECY_CONFIG__.nameplate.photo.src" />
-              </div>
-            </div>
-          </div>
-          <div class="f-s-b" v-else>
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">项目经历</div>
-              <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.experience">
-                <div class="mb-1">{{ item.date }}</div>
-                <div>
-                  {{ item.text }}
-                </div>
-              </div>
-            </div>
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">精选仓库</div>
-              <div>
-                <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.warehouse">
-                  <a :href="item.url" target="__blank">{{ item.text }}</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-            <el-button type="primary" @click="EcyUtils.Router.go({ router, path: '/home' })">Get Started</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts/edit' })">新建随笔</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts' })">管理博客</el-button>
-          </div>
-        </div>
-        <div class="w-48%">
-          <div class="mb-6">
-            <div class="font-bold title">我的技能</div>
-            <SkillGraph />
-          </div>
-          <div class="f-s-b" v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">项目经历</div>
-              <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.experience">
-                <div class="mb-1">{{ item.date }}</div>
-                <div>
-                  {{ item.text }}
-                </div>
-              </div>
-            </div>
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">精选仓库</div>
-              <div>
-                <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.warehouse">
-                  <a :href="item.url" target="__blank">{{ item.text }}</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div v-else>
-            <el-button type="primary" @click="EcyUtils.Router.go({ router, path: '/home' })">Get Started</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts/edit' })">新建随笔</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts' })">管理博客</el-button>
-          </div>
-        </div>
-      </div>
-      <div v-else>
-        <div class="mb-6">
-          <div class="mb-2 font-bold title">联系方式</div>
-          <div class="f-s-s">
-            <div class="mr-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.connection">
-              <el-tooltip :content="item.name + ': ' + item.text">
-                <svg v-if="item.svg" class="w-6 h-6" viewBox="0 0 1024 1024" v-html="item.svg"></svg>
-                <template v-else>
-                  <img class="rd-50 w-6 h-6" alt="FAILED" :src="item.img" />
-                </template>
-              </el-tooltip>
-            </div>
-          </div>
-        </div>
-        <div class="mb-6">
-          <el-button type="primary" @click="EcyUtils.Router.go({ router, path: '/home' })">Get Started</el-button>
-          <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts/edit' })">新建随笔</el-button>
-          <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts' })">管理博客</el-button>
-        </div>
-        <div class="mb-6">
-          <div class="mb-2 font-bold title">闲言碎语</div>
-          <div v-html="EcyConfig.__ECY_CONFIG__.nameplate.gossip"></div>
-        </div>
-        <div class="mb-6">
-          <div class="font-bold title">我的技能</div>
-          <SkillGraph />
-        </div>
-        <div class="f-s-b">
-          <div class="mb-6 w-50%">
-            <div class="mb-4 font-bold title">精选仓库</div>
-            <div>
-              <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.warehouse">
-                <a :href="item.url" target="__blank">{{ item.text }}</a>
-              </div>
-            </div>
-          </div>
-          <div class="mb-6 w-50%">
-            <div class="mb-4 font-bold title">项目经历</div>
-            <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.experience">
-              <div class="mb-1">{{ item.date }}</div>
-              <div>
-                {{ item.text }}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="mb-6" v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-          <div class="mb-4 font-bold title">精选图片</div>
-          <div class="f-s-b flex-wrap noscroll ofw-auto">
-            <div v-for="(item, i) in EcyConfig.__ECY_CONFIG__.nameplate.photo.src" :class="{ ' h-28': i < 3 }" style="width: 32%">
-              <el-image
-                v-if="i < 3"
-                class="w-100% h-100%"
-                :initial-index="i"
-                :src="item"
-                :preview-src-list="EcyConfig.__ECY_CONFIG__.nameplate.photo.src" />
-            </div>
+  <div id="l-cover" class="relative h-100vh w-100vw">
+    <div class="z-1 absolute top-0 left-0 h-100% w-100% f-c-c">
+      <div>
+        <div class="color-white size-2.5rem text-center">Welcome to {{ EcyConfig.blogApp }}'s blog.</div>
+        <div class="color-white size-1.2rem text-center mt-4">Time tick away, dream faded away!</div>
+        <div class="absolute bottom-10vh left-0 f-c-c w-100vw">
+          <div class="w-10 h-10 hover" @click="moveToStartNail">
+            <i-ep-arrow-down-bold class="arrow color-white size-1.5rem hover" />
           </div>
         </div>
       </div>
     </div>
-  </Card>
+    <div class="wave z-1 absolute bottom-0 left-0 h-75px w-100%">
+      <div class="wave-1 absolute h-100% w-200%"></div>
+      <div class="wave-2 absolute h-100% w-200%"></div>
+    </div>
+    <div class="cover absolute left-0 top-0 h-100% w-100%">
+      <img
+        class="z--1 h-100% w-100%"
+        src="https://ts1.cn.mm.bing.net/th/id/R-C.a5cc371a8bcd320e5b82dd1e6f1b4507?rik=Hv3Y78fE8buapg&riu=http%3a%2f%2fpic.vjshi.com%2f2017-06-13%2ff5e5a32b81bd671095ba99e58f4c0c77%2f00002.jpg%3fx-oss-process%3dstyle%2fwatermark&ehk=hZfteTa0hie3%2bwnSaJNBCz4tdtmqHG6NUw8khyOUDC4%3d&risl=&pid=ImgRaw&r=0" />
+    </div>
+  </div>
+  <div id="l-start-nail"></div>
+  <div id="l-index" class="page">
+    <div class="content">
+      <Pagination @prev="fetchData" @next="fetchData" @nexpr="fetchData" :count="listing.page" :disabled="setting.other.pagation.pin">
+        <template #content>
+          <WritingItem
+            v-if="listing.data.length > 0"
+            :padding="setting.pages.sort.padding"
+            :margin="setting.pages.sort.margin"
+            :data="listing.data" />
+        </template>
+      </Pagination>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.title {
-  --uno: l-for-size;
+#l-cover {
+  div {
+    font-family: #{"Amutham", var(--font-family)};
+  }
+
+  .cover::after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(5px);
+  }
+
+  .wave-1 {
+    top: 0;
+    left: -100%;
+    opacity: 0.5;
+    animation: move-wave-to-right 20s infinite linear;
+    background: url(https://images-1303923190.cos.ap-chengdu.myqcloud.com/wave-1.png) repeat-x;
+  }
+
+  .wave-2 {
+    top: 0;
+    left: 0%;
+    opacity: 0.6;
+    animation: move-wave-to-left 30s infinite linear;
+    background: url(https://images-1303923190.cos.ap-chengdu.myqcloud.com/wave-2.png) repeat-x;
+  }
+
+  @keyframes move-wave-to-right {
+    0% {
+      transform: translateX(0%) scaleY(1);
+    }
+    50% {
+      transform: translateX(25%) scaleY(0.85);
+    }
+    100% {
+      transform: translateX(50%) scaleY(1);
+    }
+  }
+
+  @keyframes move-wave-to-left {
+    from {
+      transform: translateX(0%);
+    }
+    to {
+      transform: translateX(-50%);
+    }
+  }
+
+  .arrow {
+    animation: move-arrow 1s infinite linear alternate;
+  }
+
+  @keyframes move-arrow {
+    0% {
+      transform: translateY(0%) scaleY(1);
+    }
+    50% {
+      transform: translateY(10%) scaleY(0.85);
+    }
+    100% {
+      transform: translateY(20%) scaleY(1);
+    }
+  }
 }
 </style>

Himmeltala/cnblogs-theme-bleu

src/views/Index.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import { useBaseAuthorData } from "@/store";
+import { WorksApi } from "@/apis";
 
 EcyUtils.startLoading();
 
-const store = useBaseAuthorData();
-const authorData = shallowRef();
-const masterData = shallowRef();
+const worksList = shallowRef(await WorksApi.getList(1));
+const imgs = EcyConfig.__ECY_CONFIG__.covers.index || ["https://img.tt98.com/d/file/tt98/201909171800581/001.jpg"];
+const covers = EcyUtils.Random.get(imgs, 0, 10);
 
-store.$onAction(({ args }) => {
-  authorData.value = args[0].author;
-  masterData.value = args[0].master;
-});
+async function fetchData(index: any) {
+  EcyUtils.startLoading();
+  WorksApi.getList(index).then(newWorksList => {
+    worksList.value = newWorksList;
+    EcyUtils.endLoading();
+  });
+}
 
-const router = useRouter();
+function moveToStartNail() {
+  document.querySelector("#l-start-nail").scrollIntoView();
+}
 
 onMounted(() => {
   EcyUtils.endLoading();
@@ -20,198 +25,57 @@ onMounted(() => {
 </script>
 
 <template>
-  <Card class="l-index min-height l-fiv-size">
-    <div class="l-index__content">
-      <div class="mb-6 f-s-c">
-        <div class="w-40%">
-          <div class="mb-6">
-            <div class="font-bold mb-2 title">{{ EcyConfig.blogApp }} 的博客</div>
-            <div :class="{ 'f-c-s': EcyConfig.pcDevice }">
-              <img
-                :class="{ 'mr-10': EcyConfig.pcDevice, 'mb-4': !EcyConfig.pcDevice }"
-                class="w-25 h-25 rd-50"
-                :src="EcyConfig.__ECY_CONFIG__.cabinet.avatar" />
-              <div>
-                <template v-if="authorData">
-                  <div class="mb-1 hover">圆龄：{{ authorData[1]?.text || "" }}</div>
-                  <div class="mb-1 hover">粉丝：{{ authorData[2]?.text || "" }}</div>
-                  <div class="mb-1 hover">关注：{{ authorData[3]?.text || "" }}</div>
-                </template>
-                <template v-if="masterData">
-                  <div class="mb-1 hover">随笔：{{ masterData[0]?.digg || "" }}</div>
-                  <div class="mb-1 hover">阅读：{{ masterData[3]?.digg || "" }}</div>
-                </template>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div class="mb-2 font-bold title">个人签名</div>
-            <div class="hover" v-html="EcyConfig.__ECY_CONFIG__.cabinet.signature"></div>
-          </div>
-        </div>
-        <div class="w-60% mb-6">
-          <div class="mb-2 font-bold title">个人简介</div>
-          <div v-html="EcyConfig.__ECY_CONFIG__.nameplate.intro"></div>
-        </div>
-      </div>
-      <div class="mb-6">
-        <div class="mb-4 font-bold title">个人标签</div>
-        <div class="hobbies f-c-s flex-wrap">
-          <LTag class="mr-2 mb-4" hover round v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.tags">{{ item }}</LTag>
-        </div>
-      </div>
-      <div v-if="EcyConfig.pcDevice" class="f-s-b mb-6">
-        <div class="w-50%">
-          <div class="mb-6">
-            <div class="mb-2 font-bold title">联系方式</div>
-            <div class="f-s-s">
-              <div class="mr-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.connection">
-                <el-tooltip :content="item.name + ': ' + item.text">
-                  <svg v-if="item.svg" class="w-6 h-6 hover" viewBox="0 0 1024 1024" v-html="item.svg"></svg>
-                  <img v-else class="rd-50 w-6 h-6 hover" alt="FAILED" :src="item.img" />
-                </el-tooltip>
-              </div>
-            </div>
-          </div>
-          <div class="mb-6">
-            <div class="mb-2 font-bold title">闲言碎语</div>
-            <div v-html="EcyConfig.__ECY_CONFIG__.nameplate.gossip"></div>
-          </div>
-          <div class="mb-6" v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-            <div class="mb-4 font-bold title">精选图片</div>
-            <div class="f-c-b flex-wrap">
-              <div v-for="(item, i) in EcyConfig.__ECY_CONFIG__.nameplate.photo.src" :class="{ ' h-28': i < 3 }" style="width: 32%">
-                <el-image
-                  v-if="i < 3"
-                  class="w-100% h-100%"
-                  :initial-index="i"
-                  :src="item"
-                  :preview-src-list="EcyConfig.__ECY_CONFIG__.nameplate.photo.src" />
-              </div>
-            </div>
-          </div>
-          <div class="f-s-b" v-else>
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">项目经历</div>
-              <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.experience">
-                <div class="mb-1">{{ item.date }}</div>
-                <div>
-                  {{ item.text }}
-                </div>
-              </div>
-            </div>
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">精选仓库</div>
-              <div>
-                <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.warehouse">
-                  <a :href="item.url" target="__blank">{{ item.text }}</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-            <el-button type="primary" @click="EcyUtils.Router.go({ router, path: '/home' })">Get Started</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts/edit' })">新建随笔</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts' })">管理博客</el-button>
-          </div>
-        </div>
-        <div class="w-48%">
-          <div class="mb-6">
-            <div class="font-bold title">我的技能</div>
-            <SkillGraph />
-          </div>
-          <div class="f-s-b" v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">项目经历</div>
-              <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.experience">
-                <div class="mb-1">{{ item.date }}</div>
-                <div>
-                  {{ item.text }}
-                </div>
-              </div>
-            </div>
-            <div class="mb-6 w-50%">
-              <div class="mb-4 font-bold title">精选仓库</div>
-              <div>
-                <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.warehouse">
-                  <a :href="item.url" target="__blank">{{ item.text }}</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div v-else>
-            <el-button type="primary" @click="EcyUtils.Router.go({ router, path: '/home' })">Get Started</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts/edit' })">新建随笔</el-button>
-            <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts' })">管理博客</el-button>
-          </div>
-        </div>
-      </div>
-      <div v-else>
-        <div class="mb-6">
-          <div class="mb-2 font-bold title">联系方式</div>
-          <div class="f-s-s">
-            <div class="mr-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.connection">
-              <el-tooltip :content="item.name + ': ' + item.text">
-                <svg v-if="item.svg" class="w-6 h-6" viewBox="0 0 1024 1024" v-html="item.svg"></svg>
-                <template v-else>
-                  <img class="rd-50 w-6 h-6" alt="FAILED" :src="item.img" />
-                </template>
-              </el-tooltip>
-            </div>
-          </div>
-        </div>
-        <div class="mb-6">
-          <el-button type="primary" @click="EcyUtils.Router.go({ router, path: '/home' })">Get Started</el-button>
-          <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts/edit' })">新建随笔</el-button>
-          <el-button text type="primary" @click="EcyUtils.Router.go({ path: 'https://i.cnblogs.com/posts' })">管理博客</el-button>
-        </div>
-        <div class="mb-6">
-          <div class="mb-2 font-bold title">闲言碎语</div>
-          <div v-html="EcyConfig.__ECY_CONFIG__.nameplate.gossip"></div>
-        </div>
-        <div class="mb-6">
-          <div class="font-bold title">我的技能</div>
-          <SkillGraph />
-        </div>
-        <div class="f-s-b">
-          <div class="mb-6 w-50%">
-            <div class="mb-4 font-bold title">精选仓库</div>
-            <div>
-              <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.warehouse">
-                <a :href="item.url" target="__blank">{{ item.text }}</a>
-              </div>
-            </div>
-          </div>
-          <div class="mb-6 w-50%">
-            <div class="mb-4 font-bold title">项目经历</div>
-            <div class="hover mb-2" v-for="item in EcyConfig.__ECY_CONFIG__.nameplate.experience">
-              <div class="mb-1">{{ item.date }}</div>
-              <div>
-                {{ item.text }}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="mb-6" v-if="!EcyConfig.__ECY_CONFIG__.nameplate.photo.disabled">
-          <div class="mb-4 font-bold title">精选图片</div>
-          <div class="f-s-b flex-wrap noscroll ofw-auto">
-            <div v-for="(item, i) in EcyConfig.__ECY_CONFIG__.nameplate.photo.src" :class="{ ' h-28': i < 3 }" style="width: 32%">
-              <el-image
-                v-if="i < 3"
-                class="w-100% h-100%"
-                :initial-index="i"
-                :src="item"
-                :preview-src-list="EcyConfig.__ECY_CONFIG__.nameplate.photo.src" />
-            </div>
-          </div>
-        </div>
+  <div class="welcome relative h-100vh w-100vw">
+    <div class="z-999 absolute bottom-15vh left-0 f-c-c w-100vw">
+      <div class="w-10 h-10 hover" @click="moveToStartNail">
+        <i-ep-arrow-down-bold class="arrow color-#ededed size-1.5rem hover" />
       </div>
     </div>
-  </Card>
+    <div class="z-999 absolute bottom-0 left-0 h-75px w-100% ofw-hidden">
+      <div class="wave-1 absolute h-100% w-200%"></div>
+      <div class="wave-2 absolute h-100% w-200%"></div>
+    </div>
+    <div class="z-990 cover absolute left-0 top-0 h-100% w-100%">
+      <img class="relative h-100% w-100% rd-0" :src="imgs[Math.floor(Math.random() * imgs.length)]" />
+    </div>
+  </div>
+  <div id="l-start-nail"></div>
+  <div id="l-index" class="page">
+    <div class="content">
+      <Pagination @prev="fetchData" @next="fetchData" @nexpr="fetchData" :count="worksList.page">
+        <template #content>
+          <WorksItem
+            v-if="worksList.data.length > 0"
+            v-for="(item, index) in worksList.data"
+            :item="item"
+            :index="index"
+            :cover="imgs[covers[index]]" />
+        </template>
+      </Pagination>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.title {
-  --uno: l-for-size;
+.welcome {
+  .cover::after {
+    backdrop-filter: blur(5px);
+  }
+
+  .arrow {
+    animation: move-arrow 1s infinite linear alternate;
+  }
+
+  @keyframes move-arrow {
+    0% {
+      transform: translateY(0%) scaleY(1);
+    }
+    50% {
+      transform: translateY(10%) scaleY(0.85);
+    }
+    100% {
+      transform: translateY(20%) scaleY(1);
+    }
+  }
 }
 </style>